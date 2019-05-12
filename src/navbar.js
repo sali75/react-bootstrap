86c41e482345c2e9224a8e4e8c0b9ca117cfdd5e
sali75 <sali75911@gmail.com>
2019-05-12 21:16:30 +0430

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -26,3 +26,46 @@ https://medium.freecodecamp.org/learn-bootstrap-4-in-30-minute-by-building-a-lan
 .nav-link:hover{ background: #f4f4f4; color: #f97300; }
 .navbar-collapse{ justify-content: flex-end;}
 .navbar-toggler{  background:#fff !important;}
+
+.navbar-collapse{
+ justify-content: flex-end;
+}
+                
+.bg-dark{
+background-color:#343a40!important
+}
+.bg-primary{
+background-color:#007bff!important
+}
+                
+                
+                
+                
+                
+                
+  <div class="row">
+   <div class="col-lg-4 col-md-4 col-sm-12">
+    <img src="images/team-3.jpg" class="img-fluid">
+    <span class="text-justify">S.Web Developer</span>
+   </div>
+   <div class="col-lg-8 col-md-8 col-sm-12 desc">
+     
+    <h3>D.John</h3>
+    <p>
+       ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+   </div>
+  </div>
+                
+                
+                
+                
+                @font-face {
+  font-family: 'MyFont';
+  src: local('MyFont'), url(./fonts/MyFont.woff) format('woff');
+}
